e4fe0eb60d72b810ce5e4ff3a57c59fc1b1f2953
theofficialhackclean <85363951+theofficialhackclean@users.noreply.github.com>
2025-03-30 03:17:29 -0400

    Update vidlink.ts

--- src/providers/sources/vidlink.ts
+++ src/providers/sources/vidlink.ts
@@ -73,7 +73,7 @@ async function vidLinkScraper(ctx: ShowScrapeContext | MovieScrapeContext): Prom
 export const vidLinkProvider = makeSourcerer({
   id: 'vidlink',
   name: 'VidLink',
-  rank: 300,
+  rank: 310,
   disabled: false,
   flags: [flags.CORS_ALLOWED],
   scrapeMovie: vidLinkScraper,
