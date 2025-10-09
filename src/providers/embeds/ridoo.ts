bcb257b4716807856279aa06ed6d9f43e61ebbfa
theofficialhackclean <85363951+theofficialhackclean@users.noreply.github.com>
2025-10-09 00:06:02 -0400

    Update ridoo.ts

--- src/providers/embeds/ridoo.ts
+++ src/providers/embeds/ridoo.ts
@@ -5,10 +5,15 @@ import { makeEmbed } from '../base';
 
 const referer = 'https://ridomovies.tv/';
 
+const playlistHeaders = {
+  referer: 'https://ridoo.net/',
+  origin: 'https://ridoo.net',
+};
+
 export const ridooScraper = makeEmbed({
   id: 'ridoo',
   name: 'Ridoo',
-  rank: 105,
+  rank: 121,
   async scrape(ctx) {
     const res = await ctx.proxiedFetcher<string>(ctx.url, {
       headers: {
@@ -25,6 +30,7 @@ export const ridooScraper = makeEmbed({
           id: 'primary',
           type: 'hls',
           playlist: url,
+          headers: playlistHeaders,
           captions: [],
           flags: [flags.CORS_ALLOWED],
         },
